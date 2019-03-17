34666da5f8698c643b0c1ebb35550e59ef69ad00
yutre021 <safedk28@gmail.com>
2019-03-16 22:49:17 -0300

    1.2

--- anjo/src/pages/page1.js
+++ anjo/src/pages/page1.js
@@ -1,40 +1,38 @@
 import React  from 'react';
+import { View, Button, Text, AppRegistry ,TextInput, StyleSheet, Platform, Image } from 'react-native';
 
-import { View, Button, Text, AppRegistry ,TextInput, StyleSheet, Platform } from 'react-native';
-
+const mainBg = require('./assets/logoHT.png')
 const page1 = ({ navigation }) => (
     
    
     <View style={styles.container}>
-    <Text style= {styles.editmenu}>Anjo</Text>
-    <View>
-      <TextInput //Inserçao de texto
-      style={{ marginBottom: 10, height: 40, borderColor: 'gray', borderWidth: 1 }}
-      onChangeText={(text) => nomeInput = text}
-      placeholder="Login"
-      
-    />
-     <TextInput //Inserçao de texto
-      style={{ marginBottom: 40, height: 40, borderColor: 'gray', borderWidth: 1 }}
-      onChangeText={(text) => nomeInput = text}
-      placeholder="Senha"
-      
-    />
-    </View>
-    <View>
-        <View style = {styles.botaoLogin}>
-        <Button 
-        
-        title="Login"
-        onPress={() => navigation.navigate('Menu')}
-    />
-    </View>
-    <Button style = {styles.botaoCadastrar}
-    
-    title="Cadastrar"
-    onPress={() => navigation.navigate('Cadastro')}
-    />
-    </View>
+        <View style={styles.containerimage}>
+            <Image
+                source={mainBg}
+                style={{ width:300 , height: 200, marginBottom: 200 }}/>  
+        </View>
+
+        <View>
+            <TextInput //Inserçao de texto
+                style={{ marginBottom: 10, height: 40, borderColor: 'gray', borderWidth: 1 }}
+                onChangeText={(text) => nomeInput = text}
+                placeholder="Login"/>
+            <TextInput //Inserçao de texto
+                style={{ marginBottom: 40, height: 40, borderColor: 'gray', borderWidth: 1 }}
+                onChangeText={(text) => nomeInput = text}
+                placeholder="Senha"/>
+        </View>
+        <View>
+            <View style = {styles.botaoLogin}>
+                <Button 
+                    title="Login"
+                    onPress={() => navigation.navigate('Menu')}/>
+            </View>
+            <Button
+                style = {styles.botaoCadastrar}
+                title="Cadastrar"
+                onPress={() => navigation.navigate('Cadastro')}/>
+        </View>
     </View>
     
 );
@@ -42,7 +40,7 @@ const page1 = ({ navigation }) => (
 // this.props.navigation.nagite('NomeDatela')
 
  page1.navigationOptions = {
-     title : '                       Bem Vindo',
+     title : '                           Bem Vindo', 
  }
 
 
@@ -54,6 +52,10 @@ const styles = StyleSheet.create({
         padding: 30,
         color: 'blue',
         justifyContent: 'flex-end'//
+    },
+    containerimage: {
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     editmenu:{
         color: 'red'
